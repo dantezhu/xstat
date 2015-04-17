9d0337c215ba0a6edb48d7aca0829b3b1e28e553
dantezhu <dantezhu@qq.com>
2015-04-17 09:33:35 +0800

    logger

--- autostat/log.py
+++ autostat/log.py
@@ -3,18 +3,3 @@
 import logging
 
 logger = logging.getLogger('autostat')
-
-import logging
-
-LOG_FORMAT = '\n'.join((
-    '/' + '-' * 80,
-    '[%(levelname)s][%(asctime)s][%(process)d:%(thread)d][%(filename)s:%(lineno)d %(funcName)s]:',
-    '%(message)s',
-    '-' * 80 + '/',
-))
-
-logger = logging.getLogger('autostat')
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter(LOG_FORMAT))
-logger.addHandler(handler)
-logger.setLevel(logging.DEBUG)
